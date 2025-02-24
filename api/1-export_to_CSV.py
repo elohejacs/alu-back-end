01c8131f3021164de3709f10d0e4622e8495bf6b
Jacobel <148277388+elohejacs@users.noreply.github.com>
2025-02-24 08:17:21 +0200

    Update 1-export_to_CSV.py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,57 +1,29 @@
 #!/usr/bin/python3
-"""
-Script to export employee task data to CSV format
-"""
-import csv
+"""Module"""
+
 import requests
 import sys
 
+if __name__ == '__main__':
+    employee_id = sys.argv[1]
+    user_url = "https://jsonplaceholder.typicode.com/users/{}" \
+        .format(employee_id)
+    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos/" \
+        .format(employee_id)
 
-def export_to_csv(employee_id):
-    """Export employee tasks to CSV file"""
-    # Base URL for the API
-    base_url = "https://jsonplaceholder.typicode.com"
+    user_info = requests.request('GET', user_url).json()
+    todos_info = requests.request('GET', todos_url).json()
 
-    # Get employee information
-    user_response = requests.get(f"{base_url}/users/{employee_id}")
-    if user_response.status_code != 200:
-        print(f"Employee with ID {employee_id} not found")
-        sys.exit(1)
+    employee_name = user_info["name"]
+    employee_username = user_info["username"]
+    task_completed = list(filter(lambda obj:
+                                 (obj["completed"] is True), todos_info))
+    number_of_done_tasks = len(task_completed)
+    total_number_of_tasks = len(todos_info)
 
-    user = user_response.json()
-    username = user.get('username')
-
-    # Get tasks for the employee
-    todos_response = requests.get(f"{base_url}/users/{employee_id}/todos")
-    if todos_response.status_code != 200:
-        print(f"Could not fetch tasks for employee {employee_id}")
-        sys.exit(1)
-
-    todos = todos_response.json()
-
-    # Write to CSV file
-    filename = f"{employee_id}.csv"
-    with open(filename, mode='w', newline='') as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        
-        # Write each task as a row in the CSV
-        for todo in todos:
-            writer.writerow([
-                str(employee_id),
-                username,
-                str(todo.get('completed')),
-                todo.get('title')
-            ])
-
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 1-export_to_CSV.py <employee_id>")
-        sys.exit(1)
-
-    try:
-        employee_id = int(sys.argv[1])
-        export_to_csv(employee_id)
-    except ValueError:
-        print("Employee ID must be an integer")
-        sys.exit(1);
+    with open(str(employee_id) + '.csv', "w") as file:
+        [file.write('"' + str(employee_id) + '",' +
+                    '"' + employee_username + '",' +
+                    '"' + str(task["completed"]) + '",' +
+                    '"' + task["title"] + '",' + "\n")
+         for task in todos_info]
