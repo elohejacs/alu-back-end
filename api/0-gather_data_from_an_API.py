d5cfa50f0cf3afab3ff1ab36d934f80d5ab0973e
Jacobel <148277388+elohejacs@users.noreply.github.com>
2025-03-10 11:29:49 +0200

    Task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -5,18 +5,19 @@ Script to fetch employee TODO list progress using an API.
 import requests
 import sys
 
+
 def fetch_todo_list(employee_id):
     """Fetch and display the TODO list progress for an employee."""
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(employee_id)).json()
-    todos = requests.get(url + "todos", params={"userId": employee_id}).json()
+    user = requests.get("{}users/{}".format(url, employee_id)).json()
+    todos = requests.get("{}todos".format(url), params={"userId": employee_id}).json()
 
-    if not user:
+    if "name" not in user:
         print("User not found")
         return
 
     employee_name = user.get("name")
-    done_tasks = [task.get("title") for task in todos if task.get("completed")]
+    done_tasks = [task["title"] for task in todos if task["completed"]]
     total_tasks = len(todos)
 
     print("Employee {} is done with tasks({}/{}):".format(
@@ -24,10 +25,9 @@ def fetch_todo_list(employee_id):
     for task in done_tasks:
         print("\t {}".format(task))
 
+
 if __name__ == "__main__":
     if len(sys.argv) != 2 or not sys.argv[1].isdigit():
         print("Usage: {} <employee_id>".format(sys.argv[0]))
     else:
         fetch_todo_list(int(sys.argv[1]))
-
-
