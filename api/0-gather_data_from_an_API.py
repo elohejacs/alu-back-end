439dcd1a83e771ccafcdbde07babcc997d5ca47c
Jacobel <148277388+elohejacs@users.noreply.github.com>
2025-03-10 11:25:34 +0200

    TAsk 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,23 +1,35 @@
 #!/usr/bin/python3
+"""
+Fetches TODO list progress for a given employee ID from an API.
+"""
 
-"""gather data from an api"""
 import requests
 import sys
 
-if __name__ == '__main__':
-    """gather data from an api"""
-    response_todos = requests.get(
-        "https://jsonplaceholder.typicode.com/user/" + sys.argv[1] + "/todos"
-    )
-    data = response_todos.json()
-    completed_tasks = []
-    for i in data:
-        if i['completed']:
-            completed_tasks.append(i['title'])
+def get_employee_todo_progress(employee_id):
+    """Fetch and display the employee's TODO list progress."""
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(employee_id)).json()
+    todos = requests.get(url + "todos", params={"userId": employee_id}).json()
+    
+    employee_name = user.get("name")
+    total_tasks = len(todos)
+    completed_tasks = [task.get("title") for task in todos if task.get("completed")]
+    
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee_name, len(completed_tasks), total_tasks))
+    for task in completed_tasks:
+        print("\t {}".format(task))
 
-    user = requests.get(
-        "https://jsonplaceholder.typicode.com/users/" + sys.argv[1]
-    ).json()
-    print("Employee {} is done with tasks({}/{}):".
-          format(user["name"], len(completed_tasks), len(data)))
-    [print("\t " + task) for task in completed_tasks]
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: {} <employee_id>".format(sys.argv[0]))
+        sys.exit(1)
+    
+    try:
+        employee_id = int(sys.argv[1])
+        get_employee_todo_progress(employee_id)
+    except ValueError:
+        print("Employee ID must be an integer.")
+        sys.exit(1)
+
